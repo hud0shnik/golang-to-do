66143ecff0f162e6e707eb10f9ae5e54eb4a3ace
Danila Egorov <c4nsero@mail.ru>
2022-03-13 15:04:33 +0300

    delete all code
    Решил вернуться к проекту и заново его переписать. Полностью.

--- user.go
+++
@@ -1,8 +0,0 @@
-package todo
-
-type User struct {
-	Id       int    `json:"-" db:"id"`
-	Name     string `json:"name" binding:"required"`
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
