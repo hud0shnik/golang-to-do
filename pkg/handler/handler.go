c13f6f90d633c0ca22f75c61a8793404575ff96f
Danila Egorov <c4nsero@mail.ru>
2022-03-15 21:14:23 +0300

    + jwt parsing
    Добавил парсинг jwt-токенов и Middleware аутентификации

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -23,7 +23,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		auth.POST("/sign-up", h.signUp)
 	}
 
-	api := router.Group("/api")
+	api := router.Group("/api", h.userIdentity)
 	{
 		lists := api.Group("/lists")
 		{
--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -1,9 +1,17 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) createList(c *gin.Context) {
+	id, _ := c.Get(userCtx)
 
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) getAllLists(c *gin.Context) {
