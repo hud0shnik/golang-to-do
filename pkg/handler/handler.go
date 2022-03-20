3ea404f333de14fa2716ed2c30c7d1e39613e221
Danila Egorov <c4nsero@mail.ru>
2022-03-20 17:24:06 +0300

    update handler.go: add comments
    Добавил комментарии к handler.go

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -6,23 +6,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Структура хендлера
 type Handler struct {
 	services *service.Service
 }
 
+// Конструктор хендлера
 func NewHandler(services *service.Service) *Handler {
 	return &Handler{services: services}
 }
 
+// Функция инициализации эндпоинтов
 func (h *Handler) InitRoutes() *gin.Engine {
+
+	// Инициализация роутера
 	router := gin.New()
 
+	// Группа эндпоинтов для аутентификации
 	auth := router.Group("/auth")
 	{
 		auth.POST("/sign-in", h.signIn)
 		auth.POST("/sign-up", h.signUp)
 	}
 
+	// Группа эндпоинтов для работы со списками
 	api := router.Group("/api", h.userIdentity)
 	{
 		lists := api.Group("/lists")
@@ -41,11 +48,14 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		}
 	}
 
+	// Группа эндпоинтов для работы с задачами списка
 	items := api.Group("items")
 	{
 		items.GET("/:id", h.getItemById)
 		items.PUT("/:id", h.updateItem)
 		items.DELETE("/:id", h.deleteItem)
 	}
+
+	// Возвращает готовый роутер
 	return router
 }
