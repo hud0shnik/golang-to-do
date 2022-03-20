25be9186bd60b4ddaa5c08bc7c0de421e81626a4
Danila Egorov <c4nsero@mail.ru>
2022-03-20 17:16:12 +0300

    + structures.go
    Переместил структуры todo и структуру user в один файл

--- structures.go
+++ structures.go
@@ -1,6 +1,16 @@
+// Файл со структурами, которые используются во всём приложении
+// Находится в корне проекта для удобства
+
 package todo
 
 import "errors"
+
+type User struct {
+	Id       int    `json:"-" db:"id"`
+	Name     string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
 type TodoList struct {
 	Id          int    `json:"id" db:"id"`
