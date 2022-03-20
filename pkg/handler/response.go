70001a39669b0bc7b2858d3b1ef161b30c3ef1c0
Danila Egorov <c4nsero@mail.ru>
2022-03-20 18:56:07 +0300

    update response.go: add comments
    Добавил комментарии response.go

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -5,6 +5,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// Структура ошибки
 type errorResponse struct {
 	Message string `json:"message"`
 }
@@ -13,7 +14,13 @@ type statusResponse struct {
 	Status string `json:"status"`
 }
 
+// Функция для обработки ошибок
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
+
+	// Вывод ошибки в консоль
 	logrus.Error(message)
+
+	// Метод, который блокирует выполнение последующих обработчиков
+	// и записывает в ответ статускод и текст ошибки
 	c.AbortWithStatusJSON(statusCode, errorResponse{message})
 }
