0b72de3b2e962ce599c5cce1523e1922647632a8
Danila Egorov <c4nsero@mail.ru>
2022-03-21 21:32:34 +0300

    update middleware.go: add comments
    Добавил комментарии к middleware.go

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -13,8 +13,13 @@ const (
 	userCtx             = "userId"
 )
 
+// Метод идентификации пользователя
 func (h *Handler) userIdentity(c *gin.Context) {
+
+	// Получение значения из хедера авторизации
 	header := c.GetHeader(authorizationHeader)
+
+	// Валидация хедера авторизации
 	if header == "" {
 		newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
 		return
@@ -25,26 +30,34 @@ func (h *Handler) userIdentity(c *gin.Context) {
 		return
 	}
 
+	// Парсинг токена
 	userId, err := h.services.Authorization.ParseToken(headerParts[1])
 	if err != nil {
 		newErrorResponse(c, http.StatusUnauthorized, err.Error())
 		return
 	}
+
+	// Запись id пользователя в контекст
 	c.Set(userCtx, userId)
 }
 
+// Функция получения id пользователя из контекста
 func getUserId(c *gin.Context) (int, error) {
+
+	// Получение пользователя из контекста
 	id, ok := c.Get(userCtx)
 	if !ok {
 		newErrorResponse(c, http.StatusInternalServerError, "user id is not found")
 		return 0, errors.New("user id not found")
 	}
 
+	// Перевод id к типу int
 	idInt, ok := id.(int)
 	if !ok {
 		newErrorResponse(c, http.StatusInternalServerError, "user id is of invalid type")
 		return 0, errors.New("user id is of invalid type")
 	}
 
+	// Возвращает полученный id и nil
 	return idInt, nil
 }
