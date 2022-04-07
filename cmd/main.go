de8c42eb697d7d15e8234398526c43d155d5b429
Danila Egorov <c4nsero@mail.ru>
2022-04-07 19:08:53 +0300

    update main.go: add comments
    Добавил комментарии к main.go

--- cmd/main.go
+++ cmd/main.go
@@ -19,16 +19,20 @@ import (
 
 func main() {
 
+	// Задаёт для логгера формат JSON
 	logrus.SetFormatter(new(logrus.JSONFormatter))
 
+	// Инициализация конфига
 	if err := initConfig(); err != nil {
 		logrus.Fatalf("error initializing config: %s", err.Error())
 	}
 
+	// Загрузка переменных окружения
 	if err := godotenv.Load(); err != nil {
 		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
 
+	// Инициализация базы данных
 	db, err := repository.NewPostgresDB(repository.Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
@@ -37,38 +41,56 @@ func main() {
 		SSLMode:  viper.GetString("db.sslmode"),
 		Password: os.Getenv("DB_PASSWORD"),
 	})
+
+	// Проверка инициализации БД
 	if err != nil {
 		logrus.Fatalf("failed to initialize db: %s", err.Error())
 	}
 
+	// Зависимости
+
+	// Слой работы с БД
 	repos := repository.NewRepository(db)
+	// Слой бизнесс логики
 	services := service.NewService(repos)
+	// Слой работы с http
 	handlers := handler.NewHandler(services)
 
+	// Создание сервера
 	srv := new(todo.Server)
+
+	// Запуск сервера в горутине
 	go func() {
 		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 			logrus.Fatalf("error occured while running http server: %s", err.Error())
 		}
 	}()
 
+	// Вывод информации о запуске приложения
 	logrus.Print("App started")
-	quit := make(chan os.Signal, 1)
 
+	// Канал для полуения сигнала о закрытии приложения
+	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 
+	// Чтение из канала, которое блокирует выполнение мейна
 	<-quit
+
+	// Вывод информации о закрытии приложения
 	logrus.Print("App shutting down")
 
+	// Остановка сервера
 	if err := srv.Shutdown(context.Background()); err != nil {
 		logrus.Errorf("error occured on server shutting down")
 	}
 
+	// Закрытие всех соединений с БД
 	if err := db.Close(); err != nil {
 		logrus.Errorf("error occured on db connection close")
 	}
 }
 
+// Функция инициализации сонфигурационного файла
 func initConfig() error {
 	viper.AddConfigPath("configs")
 	viper.SetConfigName("config")
